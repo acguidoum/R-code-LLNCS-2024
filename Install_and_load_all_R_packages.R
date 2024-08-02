2d37aff3cabd20c670ca83c001f45760ae6e57c2
A.C. GUIDOUM <15171206+acguidoum@users.noreply.github.com>
2024-08-02 23:37:22 +0100

    Add files via upload

---
+++ Install_and_load_all_R_packages.R
@@ -0,0 +1,18 @@
+# List of packages to install and load
+packages <- c("dplyr", "statip", "moments", "corrplot", "randomForest", 
+              "data.table", "ggplot2", "doParallel", "caret", "glmnet", 
+              "elasticnet", "gbm", "e1071", "nnet", "frbs", "pls", 
+              "xgboost", "monomvn", "kedd")
+
+# Function to install packages if they are not already installed
+install_if_missing <- function(p) {
+  if (!require(p, character.only = TRUE)) {
+    install.packages(p, dependencies = TRUE)
+    library(p, character.only = TRUE)
+  } else {
+    library(p, character.only = TRUE)
+  }
+}
+
+# Install and load all packages
+lapply(packages, install_if_missing)
